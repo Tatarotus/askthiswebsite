7a6fe6d683027673eda14763bce2b21550813901
Samuel Baruch <shamuelbaruch@gmail.com>
2024-08-13 23:36:51 -0300

    chat stream integration

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -3,17 +3,25 @@
 import { useChat } from "ai/react";
 
 const ChatWrapper = ({ sessionId }: { sessionId: string }) => {
-  const { messages, handleInputChange, input } = useChat({
+  const { messages, handleInputChange, handleSubmit, input } = useChat({
     api: "/api/chat-stream",
     body: { sessionId },
   });
   return (
     <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
-      <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col">
+      <div className="flex-1 text-white bg-zinc-800 justify-between flex flex-col">
         {JSON.stringify(messages)}
       </div>
 
-      <input value={input} onChange={handleInputChange} type="text" />
+      <form
+        onSubmit={handleSubmit}
+        className="bg-zinc-800 flex text-center mx-auto"
+      >
+        <input value={input} onChange={handleInputChange} type="text" />
+        <button type="submit" className="text-black bg-gray-50 px-4 mx-4">
+          send
+        </button>
+      </form>
     </div>
   );
 };
--- src/lib/rag-chat.ts
+++ src/lib/rag-chat.ts
@@ -1,5 +1,6 @@
 import { RAGChat, upstash } from "@upstash/rag-chat";
 
 export const ragChat = new RAGChat({
-  model: upstash("meta-llama/Meta-Llama-3-8B-Instruct"),
+  // model: upstash("meta-llama/Meta-Llama-3-8B-Instruct"),
+  model: upstash("mistralai/Mistral-7B-Instruct-v0.2"),
 });
