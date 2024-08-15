dca88dfb58c419961e42127ba0d2d164c9a13ea6
Samuel Baruch <shamuelbaruch@gmail.com>
2024-08-15 16:06:00 -0300

    DONE: add basic ui

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -2,27 +2,27 @@
 
 import { useChat } from "ai/react";
 import Messages from "./Messages";
+import ChatInput from "./ChatInput";
 
 const ChatWrapper = ({ sessionId }: { sessionId: string }) => {
-  const { messages, handleInputChange, handleSubmit, input } = useChat({
-    api: "/api/chat-stream",
-    body: { sessionId },
-  });
+  const { messages, handleInputChange, handleSubmit, input, setInput } =
+    useChat({
+      api: "/api/chat-stream",
+
+      body: { sessionId },
+    });
   return (
     <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
       <div className="flex-1 text-white bg-zinc-800 justify-between flex flex-col">
         <Messages messages={messages} />
       </div>
 
-      <form
-        onSubmit={handleSubmit}
-        className="bg-zinc-800 flex text-center mx-auto"
-      >
-        <input value={input} onChange={handleInputChange} type="text" />
-        <button type="submit" className="text-black bg-gray-50 px-4 mx-4">
-          send
-        </button>
-      </form>
+      <ChatInput
+        input={input}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+        setInput={setInput}
+      />
     </div>
   );
 };
